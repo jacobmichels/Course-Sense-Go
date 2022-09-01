1d70651af12ec02485cc65d3b653c8b503e3e6c9
Jacob Michels <jacob.michels2025@gmail.com>
2022-09-01 12:22:40 -0400

    add config logic

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type AppConfig struct {
+	Firestore struct {
+		ProjectID string `mapstructure:"project_id"`
+	}
+}
+
+func ReadAppConfig() (*AppConfig, error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	viper.SetDefault("firestore.project_id", "")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Println("No config file found, continuing with env and defaults")
+		} else {
+			// Config file was found but another error was produced
+			return nil, fmt.Errorf("failed to read config file: %s", err)
+		}
+	}
+
+	var config AppConfig
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %s", err)
+	}
+
+	return &config, nil
+}
